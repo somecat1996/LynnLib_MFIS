54940a61651202b787a2ec5e99804e98b45f2ec9
somecat1996 <369150573@qq.com>
2018-03-08 23:08:34 +0800

    add some functions
    IsCoprime determines whether to integrates are coprime.
    MaximumCommonFactor calculates two integrates' maximum common factor.
    Bezout computes Bézout's identity equation by Euclidean algorithm.
    Inverse needs to be finished.

--- Prime.py
+++ Prime.py
@@ -5,7 +5,12 @@
     FindPrimeAbove(x) is used to find the first prime greater 
     than x.
     FindPrimeBelow(x) is used to find the last prime less 
-    than x."""
+    than x.
+    IsCoprime(a, b) determines whether a and b are coprime.
+    MaximumCommonFactor(a, b) calculates the maximum common factor 
+    between a and b.
+    Bezout(a, b, s=False) calculates s and t which make s*a+t*b equals 
+    to 1. Set s True to show how it process."""
 from math import sqrt
 
 
@@ -58,3 +63,51 @@ def FindPrimeBelow(x):
         x = x - 1
         tmp = IsPrime(x)
     return x
+
+
+def IsCoprime(a, b):
+    if MaximumCommonFactor(a, b) == 1:
+        return True
+    return False
+
+
+def MaximumCommonFactor(a, b):
+    if not (isinstance(a, int) and isinstance(b, int)):
+        raise TypeError("Input x should be an integer.")
+    if a <= 0 or b <= 0:
+        raise ValueError("a and b should be greater than 0.")
+    r1 = a
+    r2 = b
+    while r1 % r2 != 0:
+        r1, r2 = r2, r1 % r2
+    return r2
+
+def Bezout(a, b, s=False):
+    if not (isinstance(a, int) and isinstance(b, int)):
+        raise TypeError("Input x should be an integer.")
+    if a <= 0 or b <= 0:
+        raise ValueError("a and b should be greater than 0.")
+    if not IsCoprime(a, b):
+        raise ValueError("Cannot find the s, t that satisfies the condition.")
+    q_array = []
+    r1 = a
+    r2 = b
+    while r1 % r2 != 0:
+        q_array.append(r1 / r2)
+        tmp = r1 % r2
+        if s:
+            print(str(r1)+'='+str(r1/r2)+'*'+str(r2)+'+'+str(tmp))
+        r1 = r2
+        r2 = tmp
+    s_array = [1, 0]
+    t_array = [0, 1]
+    for i in q_array:
+        s_array.append(-i * s_array[-1] + s_array[-2])
+        t_array.append(-i * t_array[-1] + t_array[-2])
+    if s:
+        print('result: '+str(s_array[-1])+"*"+str(a)+"+"+str(t_array[-1])+"*"+str(b)+"="+str(r2))
+    return s_array[-1], t_array[-1]
+
+
+def Inverse():
+    pass
